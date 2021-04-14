6f897077c53a15e4256dce19bb7d34dedf8fc159
daviaguedes <77637069+daviaguedes@users.noreply.github.com>
2021-04-14 14:36:45 -0300

    estudos java

---
+++ aula41/Aluno.java
@@ -0,0 +1,39 @@
+package aula41;
+
+public class Aluno extends Pessoa {
+	
+	private String curso;
+	private double[] notas;
+	
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia() {
+		return 0;
+	}
+	
+	public boolean verificarAprovado() {
+		return true;
+	}
+	public String obterEtiquetaEndereco() { // sobreescrita da Pessoa
+		String end = "Endereço do aluno: ";
+				end += this.getEndereco();
+		return end;
+	}
+
+	public void imprimirEtiquetaEndereco() {
+		
+		System.out.println(this.obterEtiquetaEndereco());
+	}
+	
+}
---
+++ aula42/Teste.java
@@ -0,0 +1,16 @@
+package aula42;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa = new Pessoa();
+	//	Pessoa aluno = new Aluno();
+	//	Pessoa professor = new Professor();
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+		
+	}
+
+}
